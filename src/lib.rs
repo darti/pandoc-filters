9660ff4928bf4e8a5939f6c65567a5b8f7fd8d2b
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2022-08-08 13:43:20 +0200

    Test ser/deser with echo filter

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+use pandoc_types::definition::Pandoc;
+
 pub fn dummy(input: String) -> String {
     input
 }
+
+pub fn echo(input: String) -> String {
+    let doc = serde_json::from_str::<Pandoc>(&input).unwrap();
+
+    serde_json::to_string(&doc).unwrap()
+}
+
+// pub fn repeat(input: String) -> String {
+
+// }
--- tests/simple_filter.rs
+++ tests/simple_filter.rs
@@ -1,7 +1,7 @@
 use ctor::ctor;
 use log::info;
 use pandoc::{self, OutputFormat, OutputKind};
-use pandoc_filters::dummy;
+use pandoc_filters::{dummy, echo};
 use pretty_env_logger::env_logger::{Builder, Env};
 
 #[ctor]
@@ -18,6 +18,22 @@ fn dummy_filter() {
     pandoc
         .add_input("./tests/simple_input.md")
         .add_filter(dummy)
+        .set_output(OutputKind::Pipe)
+        .set_output_format(OutputFormat::Markdown, vec![]);
+
+    match pandoc.execute().unwrap() {
+        pandoc::PandocOutput::ToBuffer(s) => info!("{}", s),
+        _ => panic!("unhandled output"),
+    }
+}
+
+#[test]
+fn echo_filter() {
+    let mut pandoc = pandoc::new();
+
+    pandoc
+        .add_input("./tests/simple_input.md")
+        .add_filter(echo)
         .set_output(OutputKind::Pipe)
         .set_output_format(OutputFormat::Markdown, vec![]);
 
